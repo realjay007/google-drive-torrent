0955d120174b41d960bc3b1e92eae43f1fd08caf
Julius Ijie <juliusijie@gmail.com>
2018-12-25 21:08:17 +0100

    Update drive torrent directory

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const DRIVE_CLIENT_ID = driveCredentials.clientId;
 const DRIVE_CLIENT_SECRET = driveCredentials.clientSecret;
 const DRIVE_REDIRECT_URI = process.env.DRIVE_REDIRECT_URI || 'http://localhost:3000/login-callback';
 const DRIVE_RETURN_FIELDS = 'id,name,webViewLink';
-const DRIVE_TORRENT_DIR = 'My torrents';
+const DRIVE_TORRENT_DIR = 'Drive Torrent';
 
 const isProduction = process.env.NODE_ENV == 'production';
 
